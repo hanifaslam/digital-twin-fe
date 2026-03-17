8afe9a546b8eea4a75c09054129708563faaa5a2
Hanif Aslam Febru Putra <aslamhanif141@gmail.com>
2026-03-17 22:06:57 +0700

    config deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   /* config options here */
-  reactCompiler: true
+  reactCompiler: true,
+
+  images: {
+    remotePatterns: [
+      // {
+      //   protocol: 'https',
+      //   hostname: 'storage-sushitei.rapier.lyr.id'
+      // }
+    ]
+  }
 }
 
 export default nextConfig
