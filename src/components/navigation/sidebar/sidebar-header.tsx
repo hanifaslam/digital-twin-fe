dcccf54025bcecf4ca4c9a3270dec232a46165bd
Hanif Aslam Febru Putra <aslamhanif141@gmail.com>
2026-03-19 11:36:33 +0700

    adjust sidebar again

--- src/components/navigation/sidebar/sidebar-header.tsx
+++ src/components/navigation/sidebar/sidebar-header.tsx
@@ -1,5 +1,3 @@
-import { PanelLeftIcon } from '@/components/icons/panel-left-icon'
-import { Button } from '@/components/ui/button'
 import ImageLoader from '@/components/wrappers/image-loader'
 import { cn } from '@/lib/utils'
 import { useSidebarStore } from '@/store/use-sidebar-store'
@@ -7,88 +5,70 @@ import Link from 'next/link'
 import React from 'react'
 
 const SidebarHeader: React.FC = () => {
-  const { isCollapsed, setIsCollapsed } = useSidebarStore()
+  const { isCollapsed } = useSidebarStore()
   return (
     <div
       className={cn(
-        'relative',
-        isCollapsed ? '' : 'flex items-center gap-2 pl-4'
+        'flex items-center transition-all duration-300 ease-in-out',
+        isCollapsed
+          ? 'h-12 px-4 pt-8 pb-2 justify-center'
+          : 'h-32 px-4 pt-2 pb-2 justify-start'
       )}
     >
-      {!isCollapsed && (
-        <Button
-          variant="ghost"
-          size="sm"
-          onClick={() => setIsCollapsed(true)}
-          className="h-12 w-12 text-muted-foreground hover:text-foreground hidden lg:flex border"
-        >
-          <PanelLeftIcon className="h-6 w-6" />
-        </Button>
-      )}
-
-      <div
+      <Link
+        href="/dashboard"
         className={cn(
           'flex items-center transition-all duration-300 ease-in-out',
-          isCollapsed
-            ? 'h-12 px-4 pt-8 pb-2 justify-center'
-            : 'h-32 px-0 pt-2 pb-2 justify-start'
+          isCollapsed ? 'gap-0' : 'gap-2'
         )}
+        draggable={false}
       >
-        <Link
-          href="/dashboard"
+        <div
           className={cn(
-            'flex items-center transition-all duration-300 ease-in-out',
-            isCollapsed ? 'gap-0' : 'gap-2'
+            'flex items-center justify-center transition-all duration-300 ease-in-out',
+            isCollapsed ? 'w-12' : 'w-16'
           )}
-          draggable={false}
         >
-          <div
-            className={cn(
-              'flex items-center justify-center transition-all duration-300 ease-in-out',
-              isCollapsed ? 'w-8' : 'w-12'
-            )}
-          >
-            <div className="dark:hidden w-full">
-              <ImageLoader
-                src="/logo.png"
-                alt="Logo"
-                width={128}
-                height={128}
-                className="object-contain w-full h-auto"
-                draggable={false}
-                loading="eager"
-              />
-            </div>
-            <div className="hidden dark:block w-full">
-              <ImageLoader
-                src="/logo.png"
-                alt="Logo"
-                width={128}
-                height={128}
-                className="object-contain w-full h-auto"
-                draggable={false}
-                loading="eager"
-              />
-            </div>
+          <div className="dark:hidden w-full">
+            <ImageLoader
+              src="/logo.png"
+              alt="Logo"
+              width={128}
+              height={128}
+              className="object-contain w-full h-auto"
+              draggable={false}
+              loading="eager"
+            />
           </div>
+          <div className="hidden dark:block w-full">
+            <ImageLoader
+              src="/logo.png"
+              alt="Logo"
+              width={128}
+              height={128}
+              className="object-contain w-full h-auto"
+              draggable={false}
+              loading="eager"
+            />
+          </div>
+        </div>
 
-          <div
-            className={cn(
-              'block transition-all duration-300 ease-in-out overflow-hidden whitespace-nowrap',
-              isCollapsed
-                ? 'max-w-0 opacity-0 -translate-x-4'
-                : 'max-w-[200px] opacity-100 translate-x-0'
-            )}
-          >
-            <div className="flex flex-col text-start">
-              <span className="text-xl font-semibold">Digital Twin</span>
-              <span className="text-sm text-muted-foreground">
-                Management System
-              </span>
-            </div>
+        <div
+          className={cn(
+            'block transition-all duration-300 ease-in-out overflow-hidden whitespace-nowrap',
+            isCollapsed
+              ? 'max-w-0 opacity-0 -translate-x-4'
+              : 'max-w-[200px] opacity-100 translate-x-0'
+          )}
+        >
+          <div className="flex flex-col text-start ml-2 ">
+            <span className="text-xl font-semibold">Digital Twin</span>
+            <span className="text-sm text-muted-foreground">
+              Management System
+            </span>
           </div>
-        </Link>
-      </div>
+        </div>
+      </Link>
     </div>
   )
 }
