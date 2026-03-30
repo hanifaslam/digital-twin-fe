86d2da55815e180929d25bf46b0e0afa26f7a409
kaynalian <krtkayullian18@gmail.com>
2026-03-30 15:32:11 +0700

    implement master building

---
+++ src/schema/master/building/building-schema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod'
+
+export const createBuildingSchema = z.object({
+  name: z.string().trim().min(1, 'Required'),
+  code: z.string().trim().min(1, 'Required'),
+  status: z.boolean().default(true)
+})
+
+export type CreateBuildingPayload = z.infer<typeof createBuildingSchema>
+
+export const updateBuildingSchema = createBuildingSchema.partial()
+
+export type UpdateBuildingPayload = z.infer<typeof updateBuildingSchema>
--- src/service/master/building/building-service.ts
+++ src/service/master/building/building-service.ts
@@ -1,14 +1,58 @@
 import { api } from '@/lib/api/axios'
 import { ApiEndpoint } from '@/lib/api/endpoint'
+import { CreateBuildingPayload, UpdateBuildingPayload } from '@/schema/master/building/building-schema'
 import { BaseParams } from '@/types/global'
-import { GetAllBuildings } from '@/types/response/master/building/building-response'
+import { GetAllBuildings, ListBuildingResponse, ShowBuildingResponse } from '@/types/response/master/building/building-response'
 
-export type BuildingListParams = BaseParams
+export interface BuildingListParams extends BaseParams {
+  status?: boolean
+  building_id?: string
 
-export const BuildingService = {
-  getAllBuildings: async () => {
-    return api.get<GetAllBuildings[]>(ApiEndpoint.MASTER.BUILDING.GET_ALL)
-  }
 }
 
-export const { getAllBuildings: getAllBuildings } = BuildingService
+export const BuildingService = {
+  list: async (params: BuildingListParams) => {
+      return api.get<ListBuildingResponse[]>(ApiEndpoint.MASTER.BUILDING.BASE, {
+        params
+      })
+    },
+  getAllBuildings: async () => {
+    return api.get<GetAllBuildings[]>(ApiEndpoint.MASTER.BUILDING.GET_ALL)
+  },
+   detail: async (id: string) => {
+      return api.get<ShowBuildingResponse>(
+        ApiEndpoint.MASTER.BUILDING.SHOW.replace(':id', id)
+      )
+    },
+  
+    create: async (data: CreateBuildingPayload) => {
+      return api.post<null>(ApiEndpoint.MASTER.BUILDING.BASE, data)
+    },
+  
+    update: async (id: string, data: UpdateBuildingPayload) => {
+      return api.patch<null>(
+        ApiEndpoint.MASTER.BUILDING.UPDATE.replace(':id', id),
+        data
+      )
+    },
+  
+    delete: async (id: string) => {
+      return api.delete<null>(ApiEndpoint.MASTER.ROOM.DELETE.replace(':id', id))
+    },
+  
+    toggleStatus: async (id: string) => {
+      return api.patch<null>(
+        ApiEndpoint.MASTER.ROOM.TOGGLE_STATUS.replace(':id', id)
+      )
+    }
+}
+
+export const {
+  list: listBuilding,
+  getAllBuildings: getAllBuildings,
+  detail: showBuilding,
+  create: createBuilding,
+  update: updateBuilding,
+  delete: deleteBuilding,
+  toggleStatus: updateBuildingStatus
+} = BuildingService
